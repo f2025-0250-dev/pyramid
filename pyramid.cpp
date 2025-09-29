213c23674ca4984012c0545c1edae002c1e66a47
Jibran <khan@MacBookAir.lan>
2025-09-29 07:14:47 +0500

    pyramid code

--- pyramid.cpp
+++ pyramid.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int rows;
-    cout << "Enter the number of rows: ";
-    cin >> rows;
+    int rows = 5; // Default number of rows
     for(int i = 1; i <= rows; ++i) {
         for(int j = 1; j <= rows - i; ++j) {
             cout << " ";
