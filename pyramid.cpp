59ab58b7c87c41d74853c0ee2fe06ad6361c7344
Jibran <khan@MacBookAir.lan>
2025-09-29 07:12:36 +0500

    full code for pyramid

--- pyramid.cpp
+++ pyramid.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-// int x = 10;
-// int y = 20;
-// int sum =x+y;
-//     cout << sum;
-
-int firstinput;
-cout << "Enter the number : ";
-cin>> firstinput;
-
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    for(int i = 1; i <= rows; ++i) {
+        for(int j = 1; j <= rows - i; ++j) {
+            cout << " ";
+        }
+        for(int k = 1; k <= 2 * i - 1; ++k) {
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
-
 }
