56a5d55c58695cf1a3319b8eea743df549d4ab98
Jibran <khan@MacBookAir.lan>
2025-09-29 07:05:11 +0500

    first changes

---
+++ pyramid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+// int x = 10;
+// int y = 20;
+// int sum =x+y;
+//     cout << sum;
+
+int firstinput;
+cout << "Enter the number : ";
+cin>> firstinput;
+
+    return 0;
+
+}
